8af8cb028a716658e50920ad89ccf9939e0930ec
Fredrik Gustafsson <fredrikg1987@gmail.com>
2023-01-17 14:41:09 +0100

    add support for component tests and add test for debugger component

---
+++ src/lib/components/Debugger/__tests__/Debugger.test.ts
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+import Debugger from '..';
+
+describe('Debugger', () => {
+	it('should render the heading text', () => {
+		render(Debugger, { entries: {} });
+		const heading = screen.getByText('Debugger');
+		expect(heading).toBeInTheDocument();
+	});
+
+	it('should render debug entries', () => {
+		render(Debugger, {
+			entries: {
+				foo: 'bar',
+				lorem: 'ipsum'
+			}
+		});
+
+		const firstKey = screen.getByText('foo:');
+		expect(firstKey).toBeInTheDocument();
+		const firstValue = screen.getByText('bar');
+		expect(firstValue).toBeInTheDocument();
+
+		const secondKey = screen.getByText('lorem:');
+		expect(secondKey).toBeInTheDocument();
+		const secondValue = screen.getByText('ipsum');
+		expect(secondValue).toBeInTheDocument();
+	});
+});
